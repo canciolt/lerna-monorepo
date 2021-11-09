e12696483a01f7913f2d78934ead7ce79d170f95
Sergio Perez <seperez@ncl.com>
2021-11-09 12:35:19 -0500

    Compile Storybook with Vite.

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,7 +1,28 @@
+const path = require('path');
+const merge = require('merge');
+const vue = require("@vitejs/plugin-vue");
+
+const resolve = (item) => path.join(__dirname, '../', item);
+
 module.exports = {
   "stories": [
     "../src/components/**/*.stories.@(js|jsx|ts|tsx)"
   ],
+  core: {
+    builder: "storybook-builder-vite"
+  },
+  viteFinal(config, { configType }) {
+    config.plugins = [...config.plugins, vue()]
+    // return the customized config
+    return merge.recursive(config, {
+      resolve: {
+        alias: {
+          '@sb': resolve('.storybook'),
+          '@components': resolve('src/components'),
+        }
+      }
+    });
+  },
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials"
