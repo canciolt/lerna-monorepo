a640a8f4b13fbb512345ce71195252193b83eea0
Sergio Perez <seperez@ncl.com>
2021-11-16 22:09:10 -0500

    Resolve lint error.

--- src/packages/modules/m62/src/m62.script.ts
+++ src/packages/modules/m62/src/m62.script.ts
@@ -2,6 +2,6 @@ import { defineComponent } from 'vue'
 import C493 from '../../../components/c493/src/c493.vue'
 
 export default defineComponent({
-  name: 'm62',
+  name: 'M62',
   components: { C493 }
 })
