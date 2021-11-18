3b40059d340c61f24287e519f5aeab1565f36dce
Sergio Perez <seperez@ncl.com>
2021-11-18 12:54:55 -0500

    Update mHeader model.

--- src/packages/modules/mHeader/src/mHeader.script.ts
+++ src/packages/modules/mHeader/src/mHeader.script.ts
@@ -1,13 +1,13 @@
 import { defineComponent } from 'vue'
 import m106 from '../../../modules/m106/src/m106.vue'
 import C1 from '../../../components/c1/src/c1.vue'
-import { Logo } from '../../../components/c1/src/c1.model'
+import { C1_LOGO_MODEL } from '../../../components/c1/src/c1.model'
 
 export default defineComponent({
   name: 'MHeader',
   components: { C1, m106 },
   props: {},
   setup () {
-    return { logoBlack: Logo.black, logoShield: Logo.shield  }
+    return { logoBlack: C1_LOGO_MODEL.black, logoShield: C1_LOGO_MODEL.shield  }
   }
 })
