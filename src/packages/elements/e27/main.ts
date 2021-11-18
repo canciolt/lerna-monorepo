db20ea8a04bc94d80b231cfcc3f95774770b9edd
Sergio Perez <seperez@ncl.com>
2021-11-18 12:32:35 -0500

    Update e27 element.

--- src/packages/elements/e27/main.ts
+++ src/packages/elements/e27/main.ts
@@ -1,1 +1,4 @@
-export { default as E27 } from './src/e27.vue'
+import E27 from './src/e27.vue'
+import { E27_LINK_MODEL, E27_MODEL } from './src/e27.model'
+
+export { E27, E27_LINK_MODEL, E27_MODEL }
--- src/packages/elements/e27/src/e27.script.ts
+++ src/packages/elements/e27/src/e27.script.ts
@@ -1,5 +1,20 @@
-import { defineComponent } from 'vue'
+import { defineComponent, PropType } from "vue";
+import { E27_LINK_MODEL } from "./e27.model";
 
 export default defineComponent({
-  name: 'E27'
+  name: 'E27',
+  props: {
+    componentClass: {
+      type: String,
+      default: ''
+    },
+    text: {
+      type: String,
+      default: ''
+    },
+    links: {
+      type: Array as PropType<E27_LINK_MODEL[]>,
+      default: []
+    }
+  }
 })
