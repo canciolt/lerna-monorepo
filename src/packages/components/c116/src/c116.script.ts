3cb87f8484443e1a4c60e17fe8ffbdc8c9a9ef03
Sergio Perez <seperez@ncl.com>
2021-11-19 18:11:40 -0500

    Add c116 component.

--- src/packages/components/c116/src/c116.script.ts
+++ src/packages/components/c116/src/c116.script.ts
@@ -1,5 +1,15 @@
 import { defineComponent } from 'vue'
 
 export default defineComponent({
-  name: 'C116'
+  name: 'C116',
+  props: {
+    componentClass: {
+      type: String,
+      default: ''
+    },
+    bodyText: {
+      type: String,
+      default: ''
+    }
+  }
 })
--- src/packages/components/c116/src/c116.stories.ts
+++ src/packages/components/c116/src/c116.stories.ts
@@ -4,9 +4,25 @@ import { Meta, Story } from '@storybook/vue3'
 import C116 from '../src/c116.vue'
 import '../src/c116.style.scss'
 
+const variants = ['-neutral', '-error', '-important', '-success', '-notify']
+
 export default {
   component: C116,
   title: 'Components/C116',
+  decorators: [() => ({ template: '<div style="min-width: 50em"><story /></div>' })],
+  parameters: {
+    layout: 'centered'
+  },
+  argTypes: {
+    componentClass: {
+      name: 'Class Modifiers',
+      options: variants,
+      control: 'select',
+    },
+    bodyText: {
+      table: { disable: true }
+    }
+  }
 } as Meta
 
 const Template: Story<Record<string, any>> = (args) => ({
@@ -18,3 +34,8 @@ const Template: Story<Record<string, any>> = (args) => ({
 })
 
 export const Default = Template.bind({})
+
+Default.args = {
+  componentClass: '-neutral',
+  bodyText: 'Incorrect username or password.'
+}
