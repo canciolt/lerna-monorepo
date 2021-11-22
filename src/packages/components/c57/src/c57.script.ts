f2a8c1be590e04fea02e258e91a9f34a29f3631d
Sergio Perez <seperez@ncl.com>
2021-11-22 09:42:43 -0500

    Update c57 component.

--- src/packages/components/c57/src/c57.script.ts
+++ src/packages/components/c57/src/c57.script.ts
@@ -1,5 +1,25 @@
-import { defineComponent } from 'vue'
+import { defineComponent, PropType } from "vue";
+import { C57_LINKS_MODEL } from "./c57.model";
 
 export default defineComponent({
-  name: 'C57'
+  name: 'C57',
+  props: {
+    componentClass: {
+      type: String,
+      default: ''
+    },
+    copyrightText: {
+      type: String,
+      default: ''
+    },
+    links: {
+      type: Array as PropType<C57_LINKS_MODEL[]>,
+      default: []
+    }
+  },
+  setup (props) {
+    const copyright = props?.copyrightText ? props?.copyrightText : `\xA9 2021 Norwegian Cruise Line Holdings Ltd.`
+
+    return { copyright }
+  }
 })
