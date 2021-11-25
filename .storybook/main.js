c7eec79ba4f24e5fcbd313b6b05e15a3c69a7218
Sergio Perez <seperez@ncl.com>
2021-11-24 23:54:05 -0500

    Add local env to Storybook.

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,40 +1,18 @@
 const merge = require('merge');
-
-const ROOT_PATH = `${__dirname }/..`
+const { loadConfigFromFile } = require('vite')
 
 module.exports = {
-  "stories": [
-    "../src/packages/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
+  stories: ["../src/packages/**/*.stories.@(js|jsx|ts|tsx)"],
   core: {
     builder: "storybook-builder-vite"
   },
-  viteFinal(config, { configType }) {
+  addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+  async viteFinal(config, { configType }) {
+    console.log(config)
+    const loadParams = {   mode: config.mode, command: 'build' }
+    const { config: viteConfig } = await loadConfigFromFile(loadParams) || {}
     config.plugins = [...config.plugins]
     // return the customized config
-    return merge.recursive(config, {
-      mode: 'development',
-      resolve: {
-        alias: {
-          '@elements': '../src/packages/elements',
-          '@components': '../src/packages/components',
-          '@modules': '.../src/packages/modules'
-        }
-      },
-      build: {
-        chunkSizeWarningLimit: 1500
-      },
-      css: {
-        preprocessorOptions: {
-          scss: {
-            additionalData: `@import "${ROOT_PATH}/src/packages/assets/src/styles/commons.scss";`
-          },
-        }
-      }
-    });
-  },
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials"
-  ]
+    return merge.recursive(config, viteConfig)
+  }
 }
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,4 @@
+document.head.insertAdjacentHTML('beforeend', `<link rel="stylesheet" href="${import.meta.env.ASSETS}global.css" />`)
 export const parameters = {
   layout: 'fullscreen'
 }
