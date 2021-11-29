4c820cebdf200fb29f484037b430afa89a2de3fe
Sergio Perez <seperez@ncl.com>
2021-11-29 15:05:19 -0500

    Update compiled styles path.

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -3,7 +3,7 @@ const Utils = {
   assetsHandler: (chunkInfo: Record<string, any>) => {
     const basePath = (chunkInfo.name.indexOf('/assets/src/fonts/') !== -1) ? 'fonts' : 'assets'
     return (chunkInfo.name === 'style.css')
-      ? 'global.css'
+      ? 'styles/global.css'
       : `${basePath}/[name].[ext]`
   }
 }
